ef24a058e712d6961ac01c1c211a55e11a3b9723
Clayton Guillot <claytonguillotjr@yahoo.com>
2018-04-09 16:08:57 -0500

    IdentityModel and Config changes

--- MC3_Music/App_Start/IdentityConfig.cs
+++ MC3_Music/App_Start/IdentityConfig.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -8,6 +7,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
+using System.Linq;
 using System.Web;
 using MC3_Music.Models;
 
@@ -16,18 +16,18 @@ namespace MC3_Music.App_Start
     public class IdentityConfig
     {
         // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
-        public class ApplicationUserManager : UserManager<ApplicationUser>
+        public class AppUserManager : UserManager<AppUser>
         {
-            public ApplicationUserManager(IUserStore<ApplicationUser> store)
+            public AppUserManager(IUserStore<AppUser> store)
                 : base(store)
             {
             }
 
-            public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
+            public static AppUserManager Create(IdentityFactoryOptions<AppUserManager> options, IOwinContext context)
             {
-                var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+                var manager = new AppUserManager(new UserStore<AppUser>(context.Get<ApplicationDbContext>()));
                 // Configure validation logic for usernames
-                manager.UserValidator = new UserValidator<ApplicationUser>(manager)
+                manager.UserValidator = new UserValidator<AppUser>(manager)
                 {
                     AllowOnlyAlphanumericUserNames = false,
                     RequireUniqueEmail = true
@@ -50,11 +50,11 @@ namespace MC3_Music.App_Start
 
                 // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
                 // You can write your own provider and plug it in here.
-                manager.RegisterTwoFactorProvider("Phone Code", new PhoneNumberTokenProvider<ApplicationUser>
+                manager.RegisterTwoFactorProvider("Phone Code", new PhoneNumberTokenProvider<AppUser>
                 {
                     MessageFormat = "Your security code is {0}"
                 });
-                manager.RegisterTwoFactorProvider("Email Code", new EmailTokenProvider<ApplicationUser>
+                manager.RegisterTwoFactorProvider("Email Code", new EmailTokenProvider<AppUser>
                 {
                     Subject = "Security Code",
                     BodyFormat = "Your security code is {0}"
@@ -65,28 +65,29 @@ namespace MC3_Music.App_Start
                 if (dataProtectionProvider != null)
                 {
                     manager.UserTokenProvider =
-                        new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
+                        new DataProtectorTokenProvider<AppUser>(dataProtectionProvider.Create("ASP.NET Identity"));
                 }
                 return manager;
             }
         }
 
         // Configure the application sign-in manager which is used in this application.
-        public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
+        public class ApplicationSignInManager : SignInManager<AppUser, string>
         {
-            public ApplicationSignInManager(ApplicationUserManager userManager, IAuthenticationManager authenticationManager)
+            public ApplicationSignInManager(AppUserManager userManager, IAuthenticationManager authenticationManager)
                 : base(userManager, authenticationManager)
             {
             }
 
-            public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
+            public override Task<ClaimsIdentity> CreateUserIdentityAsync(AppUser user)
             {
-                return user.GenerateUserIdentityAsync((ApplicationUserManager)UserManager);
+                return user.GenerateUserIdentityAsync((AppUserManager)UserManager);
             }
 
             public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
             {
-                return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
+                return new ApplicationSignInManager(context.GetUserManager<AppUserManager>(), context.Authentication);
             }
         }
+    }
 }
--- MC3_Music/Models/IdentityModels.cs
+++ MC3_Music/Models/IdentityModels.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+
 
 namespace MC3_Music.Models
 {
@@ -7,5 +11,26 @@ namespace MC3_Music.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<AppUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+        public class ApplicationDbContext : IdentityDbContext<AppUser>
+        {
+            public ApplicationDbContext()
+                : base("DefaultConnection")
+            {
+            }
+
+            public static ApplicationDbContext Create()
+            {
+                return new ApplicationDbContext();
+            }
+        }
     }
+
 }
