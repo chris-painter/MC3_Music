32fc9ee7ed3fe526b21e7bb64ed09c974ffe7c95
Chris Painter <capainter@crimson.ua.edu>
2018-04-12 12:02:51 -0500

    edited SingleAlbum

--- MC3_Music/Context/ApplicationDataContext.cs
+++ MC3_Music/Context/ApplicationDataContext.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using MC3_Music.Models;
+﻿using MC3_Music.Models;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.Data.Entity;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace MC3_Music.Context
 {
--- MC3_Music/Global.asax.cs
+++ MC3_Music/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using MC3_Music.Context;
 
 namespace MC3_Music
 {
@@ -15,6 +17,8 @@ namespace MC3_Music
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            Database.SetInitializer<ApplicationDataContext>(null);
+
         }
     }
 }
