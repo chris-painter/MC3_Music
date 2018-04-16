575fe4ff967deb181fe2dddc387d6703d9196cc6
Madison Frazer <mfrazer@crimson.ua.edu>
2018-04-16 16:03:37 -0600

    committing to make new branch

---
+++ MC3_Music/Migrations/201804162200183_SyncNew.cs
@@ -0,0 +1,16 @@
+namespace MC3_Music.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SyncNew : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
