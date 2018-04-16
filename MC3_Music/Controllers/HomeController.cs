76639e993befb811352cf92354d2eef7c49c3025
Chris Painter <chris.a.painter10@gmail.com>
2018-04-16 09:48:57 -0500

    albums in homecontroller

--- MC3_Music/Controllers/HomeController.cs
+++ MC3_Music/Controllers/HomeController.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MC3_Music.Context;
 
 namespace MC3_Music.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDataContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDataContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var albums = _context.Albums.ToList();
+
+            return View(albums);
         }
     }
 }
