92f6decf0e7530f5be3ad34066ce89bc7ddab7d1
Madison Frazer <mfrazer@crimson.ua.edu>
2018-04-22 14:32:22 -0500

    report page

--- MC3_Music/Models/Transaction.cs
+++ MC3_Music/Models/Transaction.cs
@@ -16,19 +16,20 @@ namespace MC3_Music.Models
         public Album Album { get; set; }
         public int Quantity { get; set; }
         public DateTime TransactionDate { get; set; }
-        public double price;
+        public double Price { get; set; }
 
-        public double Price
-        {
-            get
-            {
-                return price;
-            }
-            set
-            {
-                price = Album.Price;
-            }
-        }
+
+        //public double Price
+        //{
+        //    get
+        //    {
+        //        return price;
+        //    }
+        //    set
+        //    {
+        //        price = Album.Price;
+        //    }
+        //}
         
     }
 }
